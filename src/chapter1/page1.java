dfc87c777e2d7b18c5dc8f17d08679141d9047ab
Abdul Mahmoodi <ahadmahmoodi2@gmail.com>
2023-01-13 14:28:44 -0500

    sentence 2 created

--- src/chapter1/page1.java
+++ src/chapter1/page1.java
@@ -5,6 +5,7 @@ public class page1 {
     public static void main(String[] args) {
 
         System.out.println("This book is Avatar 2");
+        System.out.println("This book is written by Abdul Mahmoodi");
 
     }
 }
