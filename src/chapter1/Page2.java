1f902f15b35dfb38495975795252b9d20d4867b2
Abdul Mahmoodi <ahadmahmoodi2@gmail.com>
2023-03-09 23:35:24 -0500

    conflict

--- src/chapter1/Page2.java
+++ src/chapter1/Page2.java
@@ -7,7 +7,8 @@ public class Page2 {
         System.out.println("Avatar is very power full boy. ");
         System.out.println("Avatar was sleep for more than 100 years");
 
-
+        System.out.println("I just made some conflict");
+        System.out.println("Lets see how it is work");
 
     }
 }
